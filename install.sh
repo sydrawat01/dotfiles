a7eabc7886cad391b0fc2f23860051d1f2642faa
Siddharth Rawat <siddharth.rawat94@gmail.com>
2020-05-12 13:35:51 +0530

    add more softwares

--- install.sh
+++ install.sh
@@ -59,6 +59,21 @@ else
     echo No
 fi
 
+############# Hyper Terminal + oh-my-zsh #############
+install "############# Hyper Terminal + oh-my-zsh #############"
+echo -n "Do you wish to install Hyper Terminal with oh-my-zsh(${bold}y${reset}/${bold}n${reset})? "
+read hyperomz
+
+if [ "$hyperomz" != "${hyperomz#[Yy]}" ] ;then
+    echo Yes
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    brew cask install hyper
+    cp ./configs/.hyper.js ~/
+    cp ./runcom/.z* ~/
+else
+    echo No
+fi
+
 ############# Robo-3T #############
 install "############# Robo-3T #############"
 echo -n "Do you wish to install Robo-3T (${bold}y${reset}/${bold}n${reset})? "
@@ -110,6 +125,104 @@ if [ "$slack" != "${slack#[Yy]}" ] ;then
 else
     echo No
 fi
+
+############# Bitwarden #############
+install "############# Bitwarden #############"
+echo -n "Do you wish to install Bitwarden (${bold}y${reset}/${bold}n${reset})? "
+read bitwarden
+
+if [ "$bitwarden" != "${bitwarden#[Yy]}" ] ;then
+    echo Yes
+    brew cask install bitwarden
+else
+    echo No
+fi
+
+############# YACReader #############
+install "############# YACReader #############"
+echo -n "Do you wish to install YACReader (${bold}y${reset}/${bold}n${reset})? "
+read yacr
+
+if [ "$yacr" != "${yacr#[Yy]}" ] ;then
+    echo Yes
+    brew cask install yacreader
+else
+    echo No
+fi
+
+############# IINA Player #############
+install "############# IINA Player #############"
+echo -n "Do you wish to install IINA Player (${bold}y${reset}/${bold}n${reset})? "
+read iina
+
+if [ "$iina" != "${iina#[Yy]}" ] ;then
+    echo Yes
+    brew cask install iina
+else
+    echo No
+fi
+
+############# MongoDB Community Ed. #############
+install "############# MongoDB Community Ed. #############"
+echo -n "Do you wish to install MongoDB Community Edition (${bold}y${reset}/${bold}n${reset})? "
+read mongo
+
+if [ "$mongo" != "${mongo#[Yy]}" ] ;then
+    echo Yes
+    brew tap mongodb/brew
+    brew install mongodb-community
+else
+    echo No
+fi
+
+############# Python #############
+install "############# Python #############"
+echo -n "Do you wish to install Python (${bold}y${reset}/${bold}n${reset})? "
+read py
+
+if [ "$py" != "${py#[Yy]}" ] ;then
+    echo Yes
+    brew install python
+else
+    echo No
+fi
+
+############# Transmission #############
+install "############# Transmission #############"
+echo -n "Do you wish to install Transmission (${bold}y${reset}/${bold}n${reset})? "
+read transmission
+
+if [ "$transmission" != "${transmission#[Yy]}" ] ;then
+    echo Yes
+    brew install transmission
+else
+    echo No
+fi
+
+############# watchman #############
+install "############# watchman #############"
+echo -n "Do you wish to install watchman (${bold}y${reset}/${bold}n${reset})? "
+read watchman
+
+if [ "$watchman" != "${watchman#[Yy]}" ] ;then
+    echo Yes
+    brew install watchman
+else
+    echo No
+fi
+
+############# youtube-dl #############
+install "############# youtube-dl #############"
+echo -n "Do you wish to install youtube-dl (${bold}y${reset}/${bold}n${reset})? "
+read ydl
+
+if [ "$ydl" != "${ydl#[Yy]}" ] ;then
+    echo Yes
+    brew install youtube-dl
+else
+    echo No
+fi
+
 
 install "############# CLEANING HOMEBREW #############"
 brew cleanup
