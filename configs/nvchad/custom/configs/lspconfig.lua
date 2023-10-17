5baa90f13d90dd7f8f51d326505598a6a4659976
Siddharth Rawat <rawat.sid@northeastern.edu>
2023-10-16 23:42:27 -0400

    chore: update nvchad configs for yaml lsp

--- configs/nvchad/custom/configs/lspconfig.lua
+++ configs/nvchad/custom/configs/lspconfig.lua
@@ -17,48 +17,27 @@ end
 -- Without the loop, you would have to manually set up each LSP
 --
 
--- terraform language server setup for linting, highlights and completion
--- lspconfig.terraformls.setup{}
--- vim.api.nvim_create_autocmd({"BufWritePre"}, {
-  -- pattern = {"*.tf", "*.tfvars"},
-  -- callback = function()
-    -- vim.lsp.buf.format()
-  -- end,
--- })
--- docker, ansible and yaml language servers
--- lspconfig.dockerls.setup{}
--- lspconfig.yamlls.setup{
---   filetypes = {
---     "yaml",
---     "yaml.docker-compose"
---   }
--- }
-lspconfig.ansiblels.setup{
-  filetypes = {
-    "yaml",
-  },
+lspconfig.yamlls.setup{
+  on_attach = on_attach,
+  capabilities = capabilities,
   settings = {
-    ansible = {
-      ansible = {
-        path = "ansible",
-        useFullyQualifiedCollectionNames = true
+    yaml = {
+      schemas = {
+          kubernetes = "*.yaml",
+          ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+          ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+          ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+          ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+          ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+          ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+          ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+          ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+          ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+          ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+          ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+          ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
       },
-      ansibleLint = {
-        enabled = true,
-        path = "ansible-lint"
-      },
-      executionEnvironment = {
-        enabled = false
-      },
-      python = {
-        interpreterPath = "python"
-      },
-      completion = {
-        provideRedirectModules = true,
-        provideModuleOptionAliases = true
-      }
     },
   },
-  on_attach = on_attach,
-  capabilities = capabilities
 }
+
